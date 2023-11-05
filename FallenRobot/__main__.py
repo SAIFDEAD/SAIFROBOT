6f96be58cde627bee26586330711e0a98c87ae6f
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-11-05 10:19:05 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -405,7 +405,7 @@ def Fallen_about_callback(update: Update, context: CallbackContext):
                     ],
                     [
                         InlineKeyboardButton(
-                            text="ᴅᴇᴠᴇʟᴏᴩᴇʀ", url=f"tg://user?id={OWNER_ID}"
+                            text="ᴏᴡɴᴇʀ", url=f"tg://user?id={OWNER_ID}"
                         ),
                         InlineKeyboardButton(
                             text="ᴅᴇᴠᴇʟᴏᴘᴇʀ",
