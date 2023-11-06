20a0e5880ae0c0a9858dce4069d242c702fdcae0
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-11-06 20:56:43 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -714,7 +714,7 @@ def main():
                 chat_id=f"@{SUPPORT_CHAT}",
                 photo=START_IMG,
                 caption=f"""
-ㅤ🥀 {BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ...
+ㅤ {BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ...
 
 ┏•❅────✧❅✦❅✧────❅•┓
 ㅤ✫ **ᴍʏ ᴏᴡɴᴇʀ:**"[ᴅᴇᴀᴅ](https://t.me/DRDIC1)"
