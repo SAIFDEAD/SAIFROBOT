a9e9806dccab73e3d06086fdb8a439be2ac72db3
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-11-06 19:50:25 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -16,7 +16,7 @@ class Config(object):
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://telegra.ph/file/5fb2c06d8610256c12df5.jpg"
 
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "Dead_Groupchat"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
