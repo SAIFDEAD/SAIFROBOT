018f28701bf57427b8e312ccbb9bd87c64cbae13
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-12-26 03:09:37 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,15 +14,15 @@ class Config(object):
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://telegra.ph/file/5fb2c06d8610256c12df5.jpg"
+    START_IMG = "https://telegra.ph/file/ad01218c64c817288ec4d.jpg"
 
-    SUPPORT_CHAT = "Dead_Groupchat"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "SAIFHELPGC"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 6585111267  # User id of your telegram account (Must be integer)
+    OWNER_ID =   # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
